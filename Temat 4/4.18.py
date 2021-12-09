43e46fd97952d85b67a97c6a294c47382ead5b60
Mateusz-Soczowka <92158818+Mateusz-Soczowka@users.noreply.github.com>
2021-12-09 17:41:36 +0100

    Add files via upload

---
+++ Temat 4/4.18.py
@@ -0,0 +1,7 @@
+def NumSum(number):
+    number=str(number)
+    sum=0
+    for x in range(0,len(number)):
+        sum=sum+int(number[x])
+    return sum
+print(NumSum(7182))
---
+++ Temat 4/4.19.py
@@ -0,0 +1,6 @@
+def XYrange(number,x,y):
+    if number >= x and number <= y:
+        return True
+    else:
+        return False
+print(XYrange(88,1,100))
